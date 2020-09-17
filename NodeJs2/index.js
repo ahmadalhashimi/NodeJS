d5008646c3b558103810fa1595d2ab64d6bd4147
Ahmad Alhashimi <ahmad.alhashimi83@gmail.com>
2020-09-17 15:58:33 +0200

    NodjS_path

--- NodeJs2/index.js
+++ NodeJs2/index.js
@@ -3,12 +3,13 @@
 //npm i -D express // to stope
 
 const express = require('express')
+const path = require('path')
 
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send('Hello World')
-})
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
